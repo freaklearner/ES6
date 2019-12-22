5b90e40bb07f91cfa27cfb9150f4eed24026d262
freaklearner <shub.shubhamthakur@gmail.com>
2019-12-22 23:31:57 +0530

    require 'this' keyword while accessing own class members(both inherited and oen defined)

--- classes.js
+++ classes.js
@@ -3,6 +3,7 @@ class human{
 		this.Gender = gender;
 	}
 
+// No need to use function keyword in class while defining method member of the class
 	printGender(){
 		console.log("Gender: "+this.Gender);
 	}
@@ -19,9 +20,18 @@ class Person extends human{
 	printName(){
 		console.log("Name: "+this.Name);
 	}
+
+	printInfo(){
+		//Always need to use 'this' keyword while working with class members weather these members are inherited or not
+		// this.printName();
+		// this.printGender();
+		console.log("Name: "+this.Name);
+		console.log("Gender: "+this.Gender);
+	}
 }
 
 
 const person = new Person('test_name', "Male");
-person.printName();
-person.printGender();
+// person.printName();
+// person.printGender();
+person.printInfo();
